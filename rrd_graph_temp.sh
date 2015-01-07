153ac5ef314c0762b379af4d8bf15acd7fb32c77
Libor Gabaj <libor.gabaj@gmail.com>
2015-01-07 22:53:20 +0100

    Added new functionalities to 'rrd_graph_temp.sh'.
    - Generating a description file as a complement to respective graph file.
      It is compliant with 'Single File PHP Gallery' (http://sye.dk/sfpg/).
    - Added new script configurtion parameters for graph description file extension.
    - Added new graph configuration parameters for a RRD cumulative function and
      a graph description.

--- rrd_graph_temp.sh
+++ rrd_graph_temp.sh
@@ -20,7 +20,7 @@
 #   Their description is provided locally. Script can be configured by changing values of them.
 # - Configuration parameters in the script can be overriden by the corresponding ones
 #   in a configuration file declared in the command line.
-# - In simulation mode the script generates graph pictures to folder '/tmp'.
+# - In simulation mode the script generates graph files to folder '/tmp'.
 #
 # LICENSE:
 # This program is free software; you can redistribute it and/or modify
@@ -53,25 +53,28 @@ fi
 
 # -> BEGIN _config
 CONFIG_copyright="(c) 2014 Libor Gabaj <libor.gabaj@gmail.com>"
-CONFIG_version="0.3.0"
-CONFIG_commands=('rrdtool' 'chown') # List of commands for full running
+CONFIG_version="0.4.0"
+CONFIG_commands=('rrdtool' 'chown' 'awk' 'md5sum') # List of commands for full running
 #
 CONFIG_rrd_file="${CONFIG_script%\.*}.rrd"	# Round Robin Database file
 CONFIG_graph_dir_target=""	# Target folder for storing graph pictures
 CONFIG_graph_dir_defs=""	# Folder with graph definition files
 CONFIG_graph_ext_pic="png"	# Graph picture file extension
 CONFIG_graph_ext_def="gdf"	# Graph definition file extension
+CONFIG_graph_ext_dsc="txt"	# Graph description file extension
 CONFIG_graph_owner="www-data"	# Graph picture file owner to set after generation
 CONFIG_graph_group="www-data"	# Graph picture file group to set after generation
 # <- END _config
 
 # -> BEGIN _graph definitions
 # Placeholder for variables read from definition files and put to graph creation function
-GRAPH_name=""
+GRAPH_tag=""
 GRAPH_file_root=""
 # From definition file
 GRAPH_title=""
+GRAPH_desc=""
 GRAPH_data=""
+GRAPH_fnc=""
 GRAPH_label=""
 GRAPH_color=""
 GRAPH_width=""
@@ -98,22 +101,24 @@ $(process_help -f)
 }
 
 # @info:	Create RRD graph
-# @opts:	-t title ... name of a graph prefixed with hostname in uppercase (default none)
-#			-d data  ... name of the DS variable in RRD
-#			-l label ... descriptive name of the DS variable (default "Temperature")
-#			-c color ... color of the line in pattern RRGGBB (default "FF0000")
-#			-w width ... width of the line in pixels (default 1)
-#			-s start ... start AT-STYLE time expression (default "-1d")
+# @opts:	-t title    ... name of a graph prefixed with hostname in uppercase (default none)
+#			-d data     ... name of the DS variable in RRD
+#			-f function ... RRD cumulative function (default "AVERAGE")
+#			-l label    ... descriptive name of the DS variable (default "Temperature")
+#			-c color    ... color of the line in pattern RRGGBB (default "FF0000")
+#			-w width    ... width of the line in pixels (default 1)
+#			-s start     ... start AT-STYLE time expression (default "-1d")
 # @args:	none
 # @return:	none
 # @deps:	none
 create_graph_line_single () {
 	local file data vname
+	local function="AVERAGE"
 	local system="$(hostname)" unit="°C" time="-1d" width="1"
 	local title="${system^^*}" label="Temperature" color="FF0000"
 	local OPTIND opt
 	# Process input parameters
-	while getopts ":t:d:l:c:s:w:" opt
+	while getopts ":t:d:f:l:c:s:w:" opt
 	do
 		case "$opt" in
 		t)
@@ -122,6 +127,9 @@ create_graph_line_single () {
 		d)
 			data="$OPTARG"
 			vname="${data}_01"
+			;;
+		f)
+			function="$OPTARG"
 			;;
 		l)
 			label="$OPTARG"
@@ -144,7 +152,7 @@ create_graph_line_single () {
 	file="$1"
 	# Test variable presence
 	echo_text -fp -$CONST_level_verbose_function "Checking variable '${data}' ... "
-	rrdtool fetch "${CONFIG_rrd_file}" AVERAGE --start end+0 | head -1 | grep ${data} >/dev/null
+	rrdtool fetch "${CONFIG_rrd_file}" ${function} --start end+0 | head -1 | grep ${data} >/dev/null
 	if [ $? -eq 0 ]
 	then
 		echo_text -$CONST_level_verbose_function "valid. Proceeding."
@@ -161,7 +169,7 @@ create_graph_line_single () {
 	--vertical-label "${label} (${unit})" \
 	--alt-autoscale \
 	--alt-y-grid \
-	DEF:${data}="${CONFIG_rrd_file}":${data}:AVERAGE \
+	DEF:${data}="${CONFIG_rrd_file}":${data}:${function} \
 	CDEF:${vname}=${data},1000,/ \
 	LINE${width}:${vname}\#${color}:"${label}\c" \
 	GPRINT:${vname}:MIN:"Min\: %3.1lf${unit}" \
@@ -182,7 +190,9 @@ init_graphvars () {
 		case "$opt" in
 		I)
 			GRAPH_title="Temperature Graph"
+			GRAPH_desc=""
 			GRAPH_data="temp1"
+			GRAPH_fnc="AVERAGE"
 			GRAPH_label="Temperature"
 			GRAPH_color="FF0000"
 			GRAPH_width=1
@@ -190,7 +200,9 @@ init_graphvars () {
 			;;
 		E)
 			GRAPH_title=""
+			GRAPH_desc=""
 			GRAPH_data=""
+			GRAPH_fnc=""
 			GRAPH_label=""
 			GRAPH_color=""
 			GRAPH_width=0
@@ -268,7 +280,7 @@ then
 	echo_text -ISL -$CONST_level_verbose_none "$msg" > "$CONFIG_status"
 fi
 
-echo_text -h -$CONST_level_verbose_info "Generating graphs to '${CONFIG_graph_dir_target}' from '${CONFIG_rrd_file}':"
+echo_text -h -$CONST_level_verbose_info "Generating graph files to '${CONFIG_graph_dir_target}' from '${CONFIG_rrd_file}':"
 # Set graph file base
 if [ $CONFIG_flag_dryrun -eq 1 ]
 then
@@ -284,23 +296,38 @@ do
 	# Read graph parameters
 	init_graphvars -I
 	source "$file"
-	GRAPH_name=$(basename $file ".${CONFIG_graph_ext_def}")
-	# Create graph
-	GRAPH_file="${GRAPH_file_root}_${GRAPH_name}.${CONFIG_graph_ext_pic}"
+	GRAPH_tag=$(basename $file ".${CONFIG_graph_ext_def}")
+	# Create graph picture file
+	GRAPH_file="${GRAPH_file_root}_${GRAPH_tag}.${CONFIG_graph_ext_pic}"
 	echo_text -s -$CONST_level_verbose_info "${GRAPH_file}"
 	create_graph_line_single \
 		-t "${GRAPH_title}" \
 		-d "${GRAPH_data}" \
+		-f "${GRAPH_fnc}" \
 		-l "${GRAPH_label}" \
 		-c "${GRAPH_color}" \
 		-w "${GRAPH_width}" \
 		-s "${GRAPH_start}" \
-	"$GRAPH_file"
+	"${GRAPH_file}"
+	# Create graph description file
+	if [ -n "${GRAPH_desc}" ]
+	then
+		GRAPH_file+=".${CONFIG_graph_ext_dsc}"
+		# Chech if there is a new description
+		if [[ ! -f "${GRAPH_file}" \
+		|| "$(echo "${GRAPH_desc}" | md5sum | awk '1 {print $1}')" \
+		!= "$( md5sum "${GRAPH_file}" | awk '1 {print $1}')" ]]
+		then
+			echo "${GRAPH_desc}" > "${GRAPH_file}"
+			echo_text -s -$CONST_level_verbose_info "${GRAPH_file}"
+		fi
+	fi
 done
 
 # Change ownership of all graph files
 if [ $CONFIG_flag_dryrun -eq 0 ]
 then
 	chown ${CONFIG_graph_owner}:${CONFIG_graph_group} "${GRAPH_file_root}"*."${CONFIG_graph_ext_pic}"
+	chown ${CONFIG_graph_owner}:${CONFIG_graph_group} "${GRAPH_file_root}"*."${CONFIG_graph_ext_pic}.${CONFIG_graph_ext_dsc}"
 fi
 # End of script processed by TRAP
